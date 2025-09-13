329b70992a5d3dbb7b1c28184296f6b949594d6d
Sean Gowans <sean@Seans-MBP.fritz.box>
2025-09-13 13:33:12 +0200

    Fix Render Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'comp229-3mmr.onrender.com'
+    ],
+  },
 })
